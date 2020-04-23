bbb1798f188f28b36d6f921ff0ec1c0a08fdd87f
minnkyung <rlaalsrud458@gmail.com>
2020-04-23 21:09:32 +0900

    Assignment01

---
+++ IOS_FirstWeek_Assignment02/IOS_FirstWeek_Assignment02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_FirstWeek_Assignment02
+//
+//  Created by 민굥 on 2020/04/23.
+//  Copyright © 2020 mink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
